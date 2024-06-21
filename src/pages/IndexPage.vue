becee95a013ac98bd1a32ee456ae22d3e7e106e4
Lourenço Carlos <reincedaniel@gmail.com>
2024-06-21 15:23:11 +0100

    refactor: ups

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,6 @@
-
 <!-- MyPage.vue -->
 <template>
-    <div>
-        <template v-for="i in 2" :key="i">
-            <counter :counter-store="createCounterStore()" />
-        </template>
-    </div>
+  <div></div>
 </template>
-  
-<script>
-import { defineComponent } from 'vue'
-import { createCounterStore } from 'src/stores/store'
-import Counter from 'src/components/TestComponent.vue'
 
-export default defineComponent({
-    components: {
-        Counter
-    },
-    methods: {
-        createCounterStore() {
-            return createCounterStore()
-        }
-    }
-})
-</script>
-  
+<script></script>
