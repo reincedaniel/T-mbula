24c103229defdbc9acdc09dffa32b745505a7dc4
Lourenço Carlos <reincedaniel@gmail.com>
2024-06-25 12:41:50 +0100

    refactor: apis

--- src/services/api.js
+++ src/services/api.js
@@ -12,82 +12,30 @@ const apiClient = axios.create({
     "Access-Control-Allow-Methods": "*",
   },
 });
+const config = {
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "*",
+  },
+};
 
 export default {
   addPlace(place) {
-    return apiClient.post("/addPlace", place);
+    return apiClient.post("/addPlace", place, config);
   },
   getPlaces() {
-    var config = {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Methods": "*",
-      },
-    };
-    // return axios.get("http://localhost:3000/places", config);
-
-    return new Promise(async (resolve, reject) => {
-      try {
-        axios
-          .get(
-            `https://api-tambula.netlify.app/.netlify/functions/api/places`,
-            config
-          )
-          .then((data) => {
-            resolve({
-              code: 1,
-              message: data,
-              data,
-            });
-            /* if (data) {
-              resolve({
-                code: 1,
-                message: data,
-                data,
-              });
-            } else {
-              resolve({
-                code: 2,
-                data: data,
-                message: data,
-              });
-            } */
-          })
-          .catch(function (error) {
-            /* if (error.response) {
-              if (error.response.status === 400)
-                resolve({ code: 4, message: error });
-            } else {
-              resolve({
-                code: 5,
-                message: "Ocorreu um erro, verifique a sua conexão.",
-              });
-            } */
-            resolve({
-              code: 5,
-              message: "Ocorreu um erro, verifique a sua conexão.",
-              error,
-            });
-          });
-      } catch (error) {
-        resolve({
-          code: 4,
-          message: "Ops! Verifique a sua conexão, ocorreu um erro. ",
-          error,
-        });
-      }
-    });
+    return apiClient.get(`/places`, config);
   },
   getPlace(id) {
-    return apiClient.get(`/place/${id}`);
+    return apiClient.get(`/place/${id}`, config);
   },
   updatePlace(id, place) {
-    return apiClient.put(`/place/${id}`, place);
+    return apiClient.put(`/place/${id}`, place, config);
   },
   deletePlace(id) {
-    return apiClient.delete(`/place/${id}`);
+    return apiClient.delete(`/place/${id}`, config);
   },
 };
